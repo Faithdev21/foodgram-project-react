2979f1789f83ca9297f13ebfea0883655aa1427d
Егор Лоскутов <egorkaloskutov@yandex.ru>
2023-08-05 19:34:53 +0300

    PEP8

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -1,13 +1,12 @@
 from typing import Tuple
 
-import django_filters
-from django_filters import CharFilter
+from django_filters import CharFilter, FilterSet
 from django_filters.rest_framework import BooleanFilter
 
 from recipes.models import Ingredient, Recipe
 
 
-class RecipeFilter(django_filters.FilterSet):
+class RecipeFilter(FilterSet):
     """Фильтрация по избранному, автору и списку покупок."""
     is_favorited = BooleanFilter(field_name="is_favorited")
     is_in_shopping_cart = BooleanFilter(field_name="is_in_shopping_cart")
@@ -22,7 +21,7 @@ class RecipeFilter(django_filters.FilterSet):
         )
 
 
-class IngredientFilter(django_filters.FilterSet):
+class IngredientFilter(FilterSet):
     """Фильтрация по имени ингредиента."""
     name = CharFilter(lookup_expr='startswith')
 
